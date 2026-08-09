9fe5c5c60365aad6c829172a903ec1baf39286b0
Blaiva <blayverthreyesp14@gmail.com>
2026-08-09 11:29:31 -0400

    Crea los usecases necesarios para la pantalla de CategoriaDetalle

---
+++ app/src/main/java/com/probasketacademy/domain/usecase/jugadores/RemoverJugadorDeCategoriaUseCase.kt
@@ -0,0 +1,17 @@
+package com.probasketacademy.domain.usecase.jugadores
+
+import com.probasketacademy.domain.model.Jugador
+import com.probasketacademy.domain.repository.JugadorRepository
+import javax.inject.Inject
+
+class RemoverJugadorDeCategoriaUseCase @Inject constructor(
+    private val jugadorRepository: JugadorRepository
+) {
+    suspend operator fun invoke(jugador: Jugador) {
+        val jugadorSinCategoria = jugador.copy(
+            categoriaId = 0L,
+            categoriaNombre = ""
+        )
+        jugadorRepository.guardarJugador(jugadorSinCategoria)
+    }
+}
