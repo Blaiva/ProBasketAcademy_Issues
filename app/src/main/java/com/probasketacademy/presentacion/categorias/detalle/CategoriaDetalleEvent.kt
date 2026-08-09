aa8c2aa1a3ef259fa95921691ffa8d4b3e70ce39
Blaiva <blayverthreyesp14@gmail.com>
2026-08-09 15:53:41 -0400

    Actualiza el state y event del detalle de categoria

--- app/src/main/java/com/probasketacademy/presentacion/categorias/detalle/CategoriaDetalleEvent.kt
+++ app/src/main/java/com/probasketacademy/presentacion/categorias/detalle/CategoriaDetalleEvent.kt
@@ -6,6 +6,10 @@ sealed interface CategoriaDetalleEvent {
     data class OnCargarDetalle(val categoriaId: Long) : CategoriaDetalleEvent
     data class OnNombreCategoriaChanged(val nombre: String) : CategoriaDetalleEvent
     object OnGuardarNombreCategoria : CategoriaDetalleEvent
+    data class OnShowSaveSuccessDialogChanged(val show: Boolean) : CategoriaDetalleEvent
+    data class OnShowDeleteConfirmDialogChanged(val show: Boolean) : CategoriaDetalleEvent
+    object OnEliminarCategoria : CategoriaDetalleEvent
+    data class OnShowDeleteSuccessDialogChanged(val show: Boolean) : CategoriaDetalleEvent
     data class OnShowAddJugadoresDialogChanged(val show: Boolean) : CategoriaDetalleEvent
     data class OnJugadorSelectionToggled(val jugadorId: Long) : CategoriaDetalleEvent
     object OnAsignarJugadoresSeleccionados : CategoriaDetalleEvent
