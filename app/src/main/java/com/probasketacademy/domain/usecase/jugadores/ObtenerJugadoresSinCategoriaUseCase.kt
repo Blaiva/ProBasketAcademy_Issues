3ab96dea1a3533dd023a201ac75f6e0041dd7219
Blaiva <blayverthreyesp14@gmail.com>
2026-08-09 12:03:30 -0400

    Agrega AsignarJugadoresACategoriaUseCase.kt y ObtenerJugadoresSinCategoriaUseCase.kt

---
+++ app/src/main/java/com/probasketacademy/domain/usecase/jugadores/ObtenerJugadoresSinCategoriaUseCase.kt
@@ -0,0 +1,17 @@
+package com.probasketacademy.domain.usecase.jugadores
+
+import com.probasketacademy.domain.model.Jugador
+import com.probasketacademy.domain.repository.JugadorRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class ObtenerJugadoresSinCategoriaUseCase @Inject constructor(
+    private val jugadorRepository: JugadorRepository
+) {
+    operator fun invoke(): Flow<List<Jugador>> {
+        return jugadorRepository.obtenerJugadores().map { lista ->
+            lista.filter { it.categoriaId == 0L }
+        }
+    }
+}
