0372591edbbd228d9ef05fde90796d0d370aeb50
William Rodriguez <warodriguezv@gmail.com>
2026-08-09 19:31:14 -0400

    Implementaciones de calendario.
    Signed-off-by: William Rodriguez <warodriguezv@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,4 +118,6 @@ dependencies {
 
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
+
+    implementation(libs.kizitonwose.calendar.compose)
 }
