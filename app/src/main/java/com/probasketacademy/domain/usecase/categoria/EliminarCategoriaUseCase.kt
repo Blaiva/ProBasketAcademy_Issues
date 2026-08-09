d1969a7c74e9d756ccaec5560be3b89229ad11c6
Blaiva <blayverthreyesp14@gmail.com>
2026-08-09 15:51:27 -0400

    Agrega EliminarCategoriaUseCase.kt

---
+++ app/src/main/java/com/probasketacademy/domain/usecase/categoria/EliminarCategoriaUseCase.kt
@@ -0,0 +1,19 @@
+package com.probasketacademy.domain.usecase.categoria
+
+import com.probasketacademy.domain.repository.CategoriaRepository
+import com.probasketacademy.domain.repository.JugadorRepository
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class EliminarCategoriaUseCase @Inject constructor(
+    private val categoriaRepository: CategoriaRepository,
+    private val jugadorRepository: JugadorRepository
+) {
+    suspend operator fun invoke(categoriaId: Long) {
+        val jugadores = jugadorRepository.obtenerJugadoresPorCategoria(categoriaId).first()
+        jugadores.forEach { jugador ->
+            jugadorRepository.guardarJugador(jugador.copy(categoriaId = 0L, categoriaNombre = ""))
+        }
+        categoriaRepository.eliminarCategoria(categoriaId)
+    }
+}
