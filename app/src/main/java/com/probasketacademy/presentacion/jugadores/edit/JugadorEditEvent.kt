13159b63213743640bf9c2bcb8d26b481dd3ae41
Blaiva <blayverthreyesp14@gmail.com>
2026-08-10 18:12:15 -0400

    Crea los states y events del JugadorEdit

--- app/src/main/java/com/probasketacademy/presentacion/jugadores/edit/JugadorEditEvent.kt
+++ app/src/main/java/com/probasketacademy/presentacion/jugadores/edit/JugadorEditEvent.kt
@@ -1,5 +1,21 @@
 package com.probasketacademy.presentacion.jugadores.edit
 
 sealed interface JugadorEditEvent {
+    data class OnNombreChanged(val value: String) : JugadorEditEvent
+    data class OnTelefonoChanged(val value: String) : JugadorEditEvent
+    data class OnEdadChanged(val value: String) : JugadorEditEvent
+    data class OnDomicilioChanged(val value: String) : JugadorEditEvent
+    data class OnCategoriaNombreChanged(val value: String) : JugadorEditEvent
+    data class OnTallaCamisetaChanged(val value: String) : JugadorEditEvent
+    data class OnNumeroCamisetaChanged(val value: String) : JugadorEditEvent
+    data class OnEstaturaChanged(val value: String) : JugadorEditEvent
+    data class OnPesoChanged(val value: String) : JugadorEditEvent
+    data class OnTutorNombreChanged(val value: String) : JugadorEditEvent
+    data class OnTutorTelefonoChanged(val value: String) : JugadorEditEvent
+    data class OnTutorVinculoChanged(val value: String) : JugadorEditEvent
+    data class OnTutorCorreoChanged(val value: String) : JugadorEditEvent
+    data class OnEstadoChanged(val value: String) : JugadorEditEvent
+    data class OnDocCompletaChanged(val value: Boolean) : JugadorEditEvent
     data object OnGuardarClicked : JugadorEditEvent
+    data object OnEliminarClicked : JugadorEditEvent
 }
