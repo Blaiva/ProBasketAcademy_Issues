9dc2615ab84083a740c0d1cdd7acf331be571a0a
William Rodriguez <warodriguezv@gmail.com>
2026-08-12 19:22:47 -0400

    Mejoras del AsistenciaEvent
    Signed-off-by: William Rodriguez <warodriguezv@gmail.com>

--- app/src/main/java/com/probasketacademy/presentacion/asistencias/AsistenciasEvent.kt
+++ app/src/main/java/com/probasketacademy/presentacion/asistencias/AsistenciasEvent.kt
@@ -1,6 +1,12 @@
 package com.probasketacademy.presentacion.asistencias
 
+import java.time.LocalDate
+import java.time.YearMonth
+
 sealed interface AsistenciasEvent {
     data class OnJugadorToggled(val jugadorId: Long, val asistio: Boolean) : AsistenciasEvent
     data object OnConfirmarAsistencia : AsistenciasEvent
+    data class OnDateSelected(val date: LocalDate) : AsistenciasEvent
+    data class OnVisibleMonthChanged(val yearMonth: YearMonth) : AsistenciasEvent
+    data object OnResetGuardado : AsistenciasEvent // <-- NUEVO EVENTO PARA EVITAR EL BUG DE NAVEGACIÓN
 }
