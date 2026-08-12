dd9e8c4793cd7e5813a0d9ade133dd0072ae0bf5
William Rodriguez <warodriguezv@gmail.com>
2026-08-12 19:22:04 -0400

    Adptacion y mejoras del Repositorio de asistencia
    Signed-off-by: William Rodriguez <warodriguezv@gmail.com>

--- app/src/main/java/com/probasketacademy/data/repository/AsistenciaRepositoryImpl.kt
+++ app/src/main/java/com/probasketacademy/data/repository/AsistenciaRepositoryImpl.kt
@@ -10,15 +10,20 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class AsistenciaRepositoryImpl @Inject constructor(private val asistenciaDao: AsistenciaDao): AsistenciaRepository {
-    override fun obtenerListaAsistenciaPorCategoria(
-        categoriaId: Long,
-        fechaTimestamp: Long
-    ): Flow<List<Asistencia>> {
+    override fun obtenerListaAsistenciaPorCategoria(categoriaId: Long, fechaTimestamp: Long): Flow<List<Asistencia>> {
         return asistenciaDao.obtenerListaAsistenciaPorCategoria(categoriaId, fechaTimestamp).map { lista -> lista.map { it.toDomain(categoriaId, fechaTimestamp) } }
     }
 
     override suspend fun registrarAsistencias(asistencias: List<Asistencia>): List<Long> {
         asistenciaDao.registrarAsistencias(asistencias.map { it.toEntity() })
         return asistencias.map { it.id ?: 0 }
+    }
+
+    override fun obtenerAsistenciasPorDia(fechaTimestamp: Long): Flow<List<Asistencia>> {
+        return asistenciaDao.obtenerAsistenciasPorDia(fechaTimestamp).map { lista -> lista.map { it.toDomain() } }
+    }
+
+    override fun obtenerAsistenciaPromedioPorMes(inicio: Long, fin: Long): Flow<Double?> {
+        return asistenciaDao.obtenerAsistenciaPromedioPorMes(inicio, fin)
     }
 }
