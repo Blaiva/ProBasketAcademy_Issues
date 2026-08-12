e22f76b42bee00f6cc1afe050ff10bad757b29f8
William Rodriguez <warodriguezv@gmail.com>
2026-08-12 18:12:43 -0400

    Creacion de detalleEvent de pagos
    Signed-off-by: William Rodriguez <warodriguezv@gmail.com>

--- app/src/main/java/com/probasketacademy/presentacion/finanzas/PagosEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.probasketacademy.presentacion.finanzas
-
-sealed interface PagosEvent {
-    data object OnEnviarRecordatorio : PagosEvent
-    data object OnRegistrarPago : PagosEvent
-}
