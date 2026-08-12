1f82dedab04614ea542a9388ef29e7f3a39a6dbe
William Rodriguez <warodriguezv@gmail.com>
2026-08-12 18:12:57 -0400

    Creacion de detalleState de pagos
    Signed-off-by: William Rodriguez <warodriguezv@gmail.com>

--- app/src/main/java/com/probasketacademy/presentacion/finanzas/detalle/PagosDetalleState.kt
+++ app/src/main/java/com/probasketacademy/presentacion/finanzas/detalle/PagosDetalleState.kt
@@ -1,12 +1,13 @@
-package com.probasketacademy.presentacion.finanzas
+package com.probasketacademy.presentacion.finanzas.detalle
 
 import com.probasketacademy.domain.model.Jugador
 import com.probasketacademy.domain.model.Pago
 
-data class PagosState(
+data class PagosDetalleState(
     val isLoading: Boolean = false,
     val jugador: Jugador? = null,
     val saldoPendiente: Double = 0.0,
     val pagos: List<Pago> = emptyList(),
+    val showPagoDialog: Boolean = false,
     val errorMessage: String? = null
 )
