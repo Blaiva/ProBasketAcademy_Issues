eb200f233443ab81f826d194c2f16e14ef889233
William Rodriguez <warodriguezv@gmail.com>
2026-08-12 19:23:04 -0400

    Mejoras del AsistenciasState.kt
    Signed-off-by: William Rodriguez <warodriguezv@gmail.com>

--- app/src/main/java/com/probasketacademy/presentacion/asistencias/AsistenciasState.kt
+++ app/src/main/java/com/probasketacademy/presentacion/asistencias/AsistenciasState.kt
@@ -1,12 +1,17 @@
 package com.probasketacademy.presentacion.asistencias
 
 import com.probasketacademy.domain.model.Jugador
+import java.time.LocalDate
+import java.time.YearMonth
 
 data class AsistenciasState(
     val isLoading: Boolean = false,
-    val categoriaNombre: String = "U-16 Competitivo",
+    val categoriaNombre: String = "General",
     val jugadores: List<Jugador> = emptyList(),
     val asistencias: Map<Long, Boolean> = emptyMap(),
+    val selectedDate: LocalDate = LocalDate.now(),
+    val currentYearMonth: YearMonth = YearMonth.now(),
+    val isEditable: Boolean = true, // Controla si es hoy y se puede modificar
     val isSaved: Boolean = false,
     val errorMessage: String? = null
 )
