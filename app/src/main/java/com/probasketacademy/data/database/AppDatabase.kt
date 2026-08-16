cb25bc294623f09433f43118b45f96261ca7a1fe
William Rodriguez <warodriguezv@gmail.com>
2026-08-15 23:55:57 -0400

    Actualización de la versión y esquemas de la base de datos para soportar campos financieros y de control.
    Signed-off-by: William Rodriguez <warodriguezv@gmail.com>

--- app/src/main/java/com/probasketacademy/data/database/AppDatabase.kt
+++ app/src/main/java/com/probasketacademy/data/database/AppDatabase.kt
@@ -21,7 +21,7 @@ import com.probasketacademy.data.local.pago.PagoEntity
         PagoEntity::class,
         EventoEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase: RoomDatabase() {
