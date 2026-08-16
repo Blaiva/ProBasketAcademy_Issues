6fa3743a4b305493fddb807ea24a1af080ad0760
William Rodriguez <warodriguezv@gmail.com>
2026-08-16 00:05:48 -0400

    Adición de variables globales de generación, pagos y deudas en el estado de la lista.
    Signed-off-by: William Rodriguez <warodriguezv@gmail.com>

--- app/src/main/java/com/probasketacademy/presentacion/finanzas/list/PagosListState.kt
+++ app/src/main/java/com/probasketacademy/presentacion/finanzas/list/PagosListState.kt
@@ -6,5 +6,8 @@ data class PagosListState(
     val isLoading: Boolean = false,
     val searchQuery: String = "",
     val jugadores: List<Jugador> = emptyList(),
+    val totalGeneradoGlobal: Double = 0.0,
+    val totalPagadoGlobal: Double = 0.0,
+    val deudaGlobal: Double = 0.0,
     val errorMessage: String? = null
 )
