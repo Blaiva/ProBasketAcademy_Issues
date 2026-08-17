5a1e6c07de9a1390280ca51559b10f898b9a0e49
Blaiva <blayverthreyesp14@gmail.com>
2026-08-17 09:34:10 -0400

    Actualiza el edit y state de JugadorEdit

--- app/src/main/java/com/probasketacademy/presentacion/jugadores/edit/JugadorEditEvent.kt
+++ app/src/main/java/com/probasketacademy/presentacion/jugadores/edit/JugadorEditEvent.kt
@@ -15,8 +15,8 @@ sealed interface JugadorEditEvent {
     data class OnTutorVinculoChanged(val value: String) : JugadorEditEvent
     data class OnTutorCorreoChanged(val value: String) : JugadorEditEvent
     data class OnEstadoChanged(val value: String) : JugadorEditEvent
-    data class OnDocCompletaChanged(val value: Boolean) : JugadorEditEvent
     data class OnFotoChanged(val uri: String?) : JugadorEditEvent
+    data class OnActaNacimientoChanged(val uri: String?) : JugadorEditEvent
 
     data object OnGuardarClicked : JugadorEditEvent
     data object OnEliminarClicked : JugadorEditEvent
