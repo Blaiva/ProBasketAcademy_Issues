6e83543e53a828ff9eeb8b66bda4db0e0e1267a8
Blaiva <blayverthreyesp14@gmail.com>
2026-08-17 17:37:37 -0400

    Actualiza el dao y repositorio y su implementacion para agregar la funcion eliminar

--- app/src/main/java/com/probasketacademy/data/local/evento/EventoDao.kt
+++ app/src/main/java/com/probasketacademy/data/local/evento/EventoDao.kt
@@ -13,4 +13,7 @@ interface EventoDao {
 
     @Query("SELECT * FROM eventos WHERE fechaHoraEpocaMs BETWEEN :inicioDia AND :finDia ORDER BY fechaHoraEpocaMs ASC")
     fun obtenerEventosPorDia(inicioDia: Long, finDia: Long): Flow<List<EventoEntity>>
+
+    @Query("DELETE FROM eventos WHERE id = :id")
+    suspend fun eliminarEvento(id: Long)
 }
